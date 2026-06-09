10b130c964b1dbcdcaf878ede7107a7f0cc95710
Chris Wong <chris.wong.ksl@gmail.com>
2026-06-09 09:53:33 +0800

    Plain board

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -1,7 +1,34 @@
+import { useState } from 'react'
 import './ChessBoard.css'
 
+const SIZE = 8
+
+const initialBoardState = () => {
+  return Array(SIZE)
+    .fill(null)
+    .map(() => Array(SIZE).fill(null))
+}
+
 function ChessBoard() {
-  return <></>
+  const [board] = useState(initialBoardState())
+
+  const boxClass = () => {
+    let className = 'box'
+    return className
+  }
+  return (
+    <div className='chess-board'>
+      {board.map((row, rowIndex) => (
+        <div key={rowIndex} className='chess-row'>
+          {row.map((col, colIndex) => (
+            <div key={colIndex} className='chess-col'>
+              <div className={boxClass()}></div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default ChessBoard
