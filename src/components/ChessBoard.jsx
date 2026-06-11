d324a2cc0031c0b3820db026f9ac7c4859fa3e85
Chris Wong <chris.wong.ksl@gmail.com>
2026-06-11 13:40:25 +0800

    Move entire initialBoardState login out of component

--- src/components/ChessBoard.jsx
+++ src/components/ChessBoard.jsx
@@ -7,42 +7,40 @@ const SIZE = 8
 
 // 初始化棋盤
 const initialBoardState = () => {
-  return Array(SIZE)
+  const newBoard = Array(SIZE)
     .fill(null)
     .map(() => Array(SIZE).fill(null))
+  // 初始化黑方棋子
+  newBoard[0][0] = { type: 'rook', color: 'black' }
+  newBoard[0][1] = { type: 'knight', color: 'black' }
+  newBoard[0][2] = { type: 'bishop', color: 'black' }
+  newBoard[0][3] = { type: 'queen', color: 'black' }
+  newBoard[0][4] = { type: 'king', color: 'black' }
+  newBoard[0][5] = { type: 'bishop', color: 'black' }
+  newBoard[0][6] = { type: 'knight', color: 'black' }
+  newBoard[0][7] = { type: 'rook', color: 'black' }
+  for (let col = 0; col < SIZE; col += 1) {
+    newBoard[1][col] = { type: 'pawn', color: 'black' }
+  }
+
+  // 初始化白方棋子
+  newBoard[7][0] = { type: 'rook', color: 'white' }
+  newBoard[7][1] = { type: 'knight', color: 'white' }
+  newBoard[7][2] = { type: 'bishop', color: 'white' }
+  newBoard[7][3] = { type: 'queen', color: 'white' }
+  newBoard[7][4] = { type: 'king', color: 'white' }
+  newBoard[7][5] = { type: 'bishop', color: 'white' }
+  newBoard[7][6] = { type: 'knight', color: 'white' }
+  newBoard[7][7] = { type: 'rook', color: 'white' }
+  for (let col = 0; col < SIZE; col += 1) {
+    newBoard[6][col] = { type: 'pawn', color: 'white' }
+  }
+
+  return newBoard
 }
 
 function ChessBoard() {
-  const [board, setBoard] = useState(() => {
-    const newBoard = initialBoardState()
-    // 初始化黑方棋子
-    newBoard[0][0] = { type: 'rook', color: 'black' }
-    newBoard[0][1] = { type: 'knight', color: 'black' }
-    newBoard[0][2] = { type: 'bishop', color: 'black' }
-    newBoard[0][3] = { type: 'queen', color: 'black' }
-    newBoard[0][4] = { type: 'king', color: 'black' }
-    newBoard[0][5] = { type: 'bishop', color: 'black' }
-    newBoard[0][6] = { type: 'knight', color: 'black' }
-    newBoard[0][7] = { type: 'rook', color: 'black' }
-    for (let col = 0; col < SIZE; col += 1) {
-      newBoard[1][col] = { type: 'pawn', color: 'black' }
-    }
-
-    // 初始化白方棋子
-    newBoard[7][0] = { type: 'rook', color: 'white' }
-    newBoard[7][1] = { type: 'knight', color: 'white' }
-    newBoard[7][2] = { type: 'bishop', color: 'white' }
-    newBoard[7][3] = { type: 'queen', color: 'white' }
-    newBoard[7][4] = { type: 'king', color: 'white' }
-    newBoard[7][5] = { type: 'bishop', color: 'white' }
-    newBoard[7][6] = { type: 'knight', color: 'white' }
-    newBoard[7][7] = { type: 'rook', color: 'white' }
-    for (let col = 0; col < SIZE; col += 1) {
-      newBoard[6][col] = { type: 'pawn', color: 'white' }
-    }
-
-    return newBoard
-  })
+  const [board, setBoard] = useState(() => initialBoardState())
   const [whoseTurn, setWhoseTurn] = useState('white')
   const [selectedStatus, setSelectedStatus] = useState({
     piece: {},
